d9f7897854bb7a35251157abf0942e533794f6d4
halehfoto <28.haleh@gmail.com>
2023-02-27 14:44:13 -0500

    Update SpeedUp.m

--- SpeedUp.m
+++ SpeedUp.m
@@ -3,9 +3,23 @@
 clearvars
 close all
 path='\\research.files.med.harvard.edu\Wyss Institute\Levin Lab\Haleh';
-speedup=input('please enter how much you like to speed up the video, e.g. for 10 times enter 10:') 
 cd(path)
+path2=uigetdir;
+cd(path2)
 filename=uigetfile;
-v=VideoReader(filename);
-T0=v.CurrentTime;
-fps=v.Framerate;
+speedup=input('Please enter the time interval you need in between frames in seconds:') 
+v1=VideoReader(filename);
+
+v2=VideoWriter(strcat(num2str(speedup),'_secinterval',filename));
+open(v2);
+i=1;
+while hasFrame(v1)
+    if i<v1.NumFrames
+        k=read(v1,i);
+        v2.writeVideo(k)
+    else
+        break;
+    end
+    i=i+round(speedup*v1.FrameRate);
+end
+close(v2);
